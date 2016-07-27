8d3110dda41a1131823b7557fe38a1c2a2bbbd86
Ramiro Pinol <ramirognr@gmail.com>
2016-07-27 22:53:21 +0000

    mongodb code cleanup

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ var url = "mongodb://localhost:27017/test"
 var async = require("async");
 
 //TO-DO: ver como parar la ejecucion y devolver valor a res.end()
-//       ver la forma de limpiar todos los mongo.connect de cada funcion
 
 function main(input, callback) {
     var input = input;
-    async.waterfall([
-        
+    var link;
+    mongo.connect(url, function(err, db) {
+        if (err) throw err;
+        var links = db.collection('links');
+        async.waterfall([
+
         // Check if input is new URL (true) or short link (false)
         function (callback) {
             var is_url = isNaN(Number(input));
@@ -20,16 +23,10 @@ function main(input, callback) {
         // If input is short_link, returns original_url
         function (is_url, callback) {
             if (!is_url) {
-                mongo.connect(url, function(err, db) {
+                links.find( {"short_url" : "https://little-url.herokuapp.com/" + input} ).toArray(function(err, data) {
+                    link = data[0].original_url;
                     if (err) throw err;
-                    var links = db.collection('links');
-                    links.find( {"short_url" : "https://little-url.herokuapp.com/" + input} ).toArray(function(err, data) {
-                        var link = data[0].original_url;
-                        if (err) throw err;
-                        console.log(link);
-                        callback(true, link)
-                    });
-                    db.close();
+                    callback(null, link);
                 });
             } else {
                 callback(null);
@@ -39,64 +36,57 @@ function main(input, callback) {
         // Searches for URL in DB, return it if exists
         function (callback) {
             var clean = input.toString().slice(4);
-            mongo.connect(url, function(err, db) {
+            links.find({ "original_url" : clean }).toArray(function(err, data) {
                 if (err) throw err;
-                var links = db.collection('links');
-                links.find({ "original_url" : clean }).toArray(function(err, data) {
-                    if (err) throw err;
-                    if (data != 0) {
-                        //Aca deberia devolver data
-                        console.log(data[0]);
-                    } else {
-                        callback (null, clean);
-                    }
-                });
-                db.close();
-            })
+                if (data != 0) {
+                    //Aca deberia devolver original_url
+                    var original_url = data[0];
+                    callback(null, original_url);
+                } else {
+                    callback(null, clean);
+                }
+            });
         },
         
         // Count URLs in database to set next URL ID/short_link number
         function (clean, callback) {
-            mongo.connect(url, function(err, db) {
-                if (err) throw err
-                var links = db.collection('links');
-                links.count(function (err, countLinks) {
-                    if (err) throw err;
-                    callback(null, clean, countLinks)
-                })
-                db.close()
+            links.count(function (err, countLinks) {
+                if (err) throw err;
+                callback(null, clean, countLinks)
             })
         },
         
         //Adds non-existent URL to DB and returns the object
         function (clean, countLinks, callback) {
             var countLinks = countLinks + 1;
-            mongo.connect(url, function(err, db) {
-                if (err) throw err
-                var links = db.collection('links');
-                links.insert({
-                        _id : countLinks,
-                        "original_url": clean,
-                        "short_url" : "https://little-url.herokuapp.com/" + countLinks
-                        }, function(err, result) {
-                            if (err) throw err;
-                            console.log("URL added to DB");
-                            //Esto tengo que devolver
-                            console.log(result.ops[0])
-                            callback(null);
-                })
+            links.insert({
+                    _id : countLinks,
+                    "original_url": clean,
+                    "short_url" : "https://little-url.herokuapp.com/" + countLinks
+                    }, function(err, result) {
+                        if (err) throw err;
+                        console.log("URL added to DB");
+                        //Esto tengo que devolver
+                        var json = result.ops[0]
+                        callback(null, json);
             })
         },
    
     ], function (error, success) {
-        if (error) throw success;
-        return console.log('Done!');
-    });
+        if (error) {
+            console.log(error)
+        } else {
+            console.log(success)
+        }
+        });
+    })
 };
 
 app.get('/:url*', function (req, res) {
     var dire = req.url.slice(1);
-    res.send(main(dire));
+    var out = main(dire);
+    console.log(out);
+    //res.send(main(dire));
 });
 
 app.listen(process.env.PORT || 8080, function () {
