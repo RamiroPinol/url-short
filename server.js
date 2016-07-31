9856111fb3a676632d840e2812ce20e3a113994e
Ramiro Pinol <ramirognr@gmail.com>
2016-07-31 02:22:08 +0000

    bugfixes

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var mongo = require("mongodb").MongoClient
 var url = "mongodb://localhost:27017/test"
 var async = require("async");
 var validator = require("validator");
+var crypto = require('crypto');
+
+// Creates a 4-length hash from SHA256 to use as short-links/ID
+function hash(id) {
+  var hash = crypto.createHash('sha256')
+        .update(new Date().getTime().toString() + "" + id)
+        .digest('hex')
+        return(hash.slice(4, 8));
+}
 
 function main(input, res, callback) {
     var input = input;
@@ -15,10 +24,9 @@ function main(input, res, callback) {
 
         function (callback) {
             // Check if input is new URL (true) or short link (false)
-            var is_url = isNaN(Number(input));
-            
-            // Return original URL if input is short link
-            if (!is_url) {
+            // by checking if input in alphanumeric (URLs has symbols).
+            // Then return original URL if input is short link
+            if (validator.isAlphanumeric(input, ["en-US"])) {
                 links.find( {"short_url" : "https://little-url.herokuapp.com/" + input} ).toArray(function(err, data) {
                     link = data[0].original_url;
                     if (err) throw err;
@@ -28,8 +36,7 @@ function main(input, res, callback) {
             } else {
                 
                 //Return data json if URL already exists in DB
-                var clean = input.toString().slice(4);
-                links.find({ "original_url" : clean }).toArray(function(err, data) {
+                links.find({ "original_url" : input }).toArray(function(err, data) {
                     if (err) throw err;
                     if (data != 0) {
                         var original_url = data[0];
@@ -40,11 +47,11 @@ function main(input, res, callback) {
                         // If URL not in DB, adds it and return data json
                         links.count(function (err, countLinks) {
                             if (err) throw err;
-                            var countLinks = countLinks + 1;
+                            var id = hash(countLinks + 1);
                             links.insert({
-                                    _id : countLinks,
-                                    "original_url": clean,
-                                    "short_url" : "https://little-url.herokuapp.com/" + countLinks
+                                    _id : id,
+                                    "original_url": input,
+                                    "short_url" : "https://little-url.herokuapp.com/" + id
                                     }, function(err, result) {
                                         if (err) throw err;
                                         console.log("URL added to DB");
@@ -57,27 +64,30 @@ function main(input, res, callback) {
                 })
             }
         },
-
+    // Result of main can be only string (URL) or JSON
+    // so callback function redirects to URL is response is str
+    // send JSON otherwise
     ], function (error, success) {
         if (error) {
             console.log(error)
         } else {
             var success = success[0];
-            res.send(success);
+            if (typeof success == "string") {
+                res.redirect(success);
+            } else {
+                res.send(success);
+            }
         }
         });
     })
 };
 
-        
 app.get('/:url*', function (req, res) {
     var input = req.url.slice(1);
-    var is_url = isNaN(Number(input));
-    if (!is_url) {
+    if (validator.isAlphanumeric(input, ["en-US"])) {
         main(input, res);
-        console.log(input)
     } else {
-        var div = input.indexOf("http://");
+        var div = input.indexOf("/") + 1;
         var pre = input.slice(0, div);
         var url = input.slice(div);
         //validator validates some bad URLs
